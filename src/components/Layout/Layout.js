b483535fe06a342b0e0b0a3c7ddd0545662e540f
Prakash Thakur <prakashthakur0021@gmail.com>
2020-06-19 17:03:28 -0700

    creating responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxilliary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Backdrop from "../UI/Backdrop/Backdrop";
 
-const Layout = (props) => {
-  return (
-    <Aux>
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-    </Aux>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
