e296cb42732c5bc4b1457b76025fe972ca2f1917
Prakash Thakur <prakashthakur0021@gmail.com>
2020-10-24 03:00:58 +0530

    Implemented using Redux(2)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,17 +39,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseConitnueHandler = () => {
-    // alert("You Choose to Continue");
-    const queryParams = [];
-    for(let i in this.state.ingredients){
-      queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]))
-    }
-    queryParams.push('price='+ this.state.totPrice);
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString,
-    })
+    this.props.history.push('/checkout')
   };
 
   updatePurchasable = (ingredients) => {
